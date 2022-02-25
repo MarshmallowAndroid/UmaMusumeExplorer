de1f2edf5fb8b62b0e2cf5fe32b86e1d17499907
Jacob Tarun <jacobtarun782@outlook.com>
2022-02-26 01:29:31 +0800

    Switch back to AssetStudio Texture2DDecoder

--- UmaMusumeExplorer/Controls/CharacterInfo/UnityTextureHelpers.cs
+++ UmaMusumeExplorer/Controls/CharacterInfo/UnityTextureHelpers.cs
@@ -1,5 +1,4 @@
 ﻿using AssetStudio;
-using open.imaging.S3TC;
 using PlayerGui.Controls.CharacterInfo;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -33,54 +32,14 @@ namespace UmaMusumeExplorer.Controls.CharacterInfo
             SerializedFile targetAsset = assetsManager.assetsFileList.Where(
                 a => ((NamedObject)a.Objects.Where(o => o.type == ClassIDType.Texture2D).First()).m_Name.StartsWith(imageString)).First();
             Texture2D texture = (Texture2D)targetAsset.Objects.Where(o => o.type == ClassIDType.Texture2D).First();
-            Image<Bgra32> image = ConvertToImage(texture, true);
+
+            Image<Bgra32> image = texture.ConvertToImage(true);
 
             int adjustedHeight = (int)(image.Height * 1.10f);
             image.Mutate(o => o.Resize(image.Width, adjustedHeight));
 
-            PinnedBitmap bitmap = new(ConvertToBgra32Bytes(image), image.Width, adjustedHeight);
+            PinnedBitmap bitmap = new(image.ConvertToBgra32Bytes(), image.Width, adjustedHeight);
             return bitmap.Bitmap;
-        }
-
-        private static unsafe Image<Bgra32> ConvertToImage(Texture2D m_Texture2D, bool flip)
-        {
-            var buff = BigArrayPool<byte>.Shared.Rent(m_Texture2D.m_Width * m_Texture2D.m_Height * 4);
-            var buff2 = BigArrayPool<byte>.Shared.Rent(m_Texture2D.image_data.Size);
-            m_Texture2D.image_data.GetData(buff2);
-
-            try
-            {
-                fixed (byte* data = buff2)
-                {
-                    fixed (byte* image = buff)
-                    {
-                        DXT5Decoder.Decode((uint)m_Texture2D.m_Width, (uint)m_Texture2D.m_Height, data, image);
-                    }
-                }
-                if (buff.Length > 0)
-                {
-                    var image = Image.LoadPixelData<Bgra32>(buff, m_Texture2D.m_Width, m_Texture2D.m_Height);
-                    if (flip)
-                    {
-                        image.Mutate(x => x.Flip(FlipMode.Vertical));
-                    }
-                    return image;
-                }
-                return null;
-            }
-            finally
-            {
-                BigArrayPool<byte>.Shared.Return(buff);
-            }
-        }
-
-        private static byte[] ConvertToBgra32Bytes(Image<Bgra32> image)
-        {
-            if (image.DangerousTryGetSinglePixelMemory(out var pixelMemory))
-            {
-                return MemoryMarshal.AsBytes(pixelMemory.Span).ToArray();
-            }
-            return null;
         }
     }
 }
