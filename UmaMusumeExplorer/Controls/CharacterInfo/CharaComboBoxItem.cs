a577e8ef340276c8d8dd5715e04c64e1d6b23e74
Jacob Tarun <jacobtarun782@outlook.com>
2022-02-28 01:23:19 +0800

    Separate combo box item

---
+++ UmaMusumeExplorer/Controls/CharacterInfo/CharaComboBoxItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UmaMusumeFiles.Tables;
+using UmaMusumeExplorer.Controls;
+
+namespace PlayerGui.Controls.CharacterInfo
+{
+    internal class CharaComboBoxItem
+    {
+        public CharaComboBoxItem(CharaData chara)
+        {
+            CharaData = chara;
+        }
+
+        public CharaData CharaData { get; }
+
+        public override string ToString()
+        {
+            return CharaData.Id.ToString() + ": " + PersistentData.CharaNameTextDatas.First(td => td.Index == CharaData.Id).Text;
+        }
+    }
+}
--- UmaMusumeExplorer/Controls/CharacterInfo/CharacterInfoControl.cs
+++ UmaMusumeExplorer/Controls/CharacterInfo/CharacterInfoControl.cs
@@ -119,19 +119,4 @@ namespace PlayerGui.Controls.CharacterInfo
         }
 
     }
-
-    internal class CharaComboBoxItem
-    {
-        public CharaComboBoxItem(CharaData chara)
-        {
-            CharaData = chara;
-        }
-
-        public CharaData CharaData { get; }
-
-        public override string ToString()
-        {
-            return CharaData.Id.ToString() + ": " + PersistentData.CharaNameTextDatas.First(td => td.Index == CharaData.Id).Text;
-        }
-    }
 }
