41d720b4649e34707c43e5733d576ba576bde83c
Jacob Tarun <jacobtarun782@outlook.com>
2022-02-28 03:53:41 +0800

    Fix slow loading regression

--- UmaMusumeExplorer/Controls/CharacterInfo/CharacterInfoControl.cs
+++ UmaMusumeExplorer/Controls/CharacterInfo/CharacterInfoControl.cs
@@ -32,10 +32,10 @@ namespace PlayerGui.Controls.CharacterInfo
             CharaData chara = charaIcon.Tag as CharaData;
 
             CardDetailsForm details = new(chara);
-            OpenFormInParentCenter(details);
+            ShowFormInParentCenter(details);
         }
 
-        private void LoadingBackgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        private void LoadingBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
             Regex charIconRegex = new("\\bchr_icon_[0-9]{4}\\b");
 
@@ -49,51 +49,49 @@ namespace PlayerGui.Controls.CharacterInfo
             AssetStudio.Progress.Default = new AssetStudioProgress(loadingBackgroundWorker.ReportProgress);
             UnityTextureHelpers.LoadFiles(imagePaths.ToArray());
 
+            List<Control> pictureBoxes = new();
+
             int itemNumber = 1;
             foreach (var item in charaDatas)
             {
-                Invoke(() => AddItems(item));
+                PictureBox charaIcon = new()
+                {
+                    Width = 100,
+                    Height = 100,
+                    BackgroundImage = UnityTextureHelpers.GetCharaIcon(item.Id),
+                    BackgroundImageLayout = ImageLayout.Zoom,
+                    Cursor = Cursors.Hand,
+                    Tag = item
+                };
+                charaIcon.Click += CharaIcon_Click;
+                pictureBoxes.Add(charaIcon);
+
+                Invoke(() => charaListComboBox.Items.Add(new CharaComboBoxItem(item)));
 
                 loadingBackgroundWorker.ReportProgress((int)((float)itemNumber++ / charaDatas.Count() * 100.0f));
             }
+
+            e.Result = pictureBoxes.ToArray();
         }
 
-        private void LoadingBackgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        private void LoadingBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             loadingProgressBar.Value = e.ProgressPercentage;
         }
 
-        private void LoadingBackgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        private void LoadingBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             loadingProgressBar.Visible = false;
+
+            charactersPanel.Controls.AddRange((Control[])e.Result);
         }
 
         private void GoButton_Click(object sender, EventArgs e)
         {
-            CharaComboBoxItem item = charaListComboBox.SelectedItem as CharaComboBoxItem;
-            if (item != null) OpenFormInParentCenter(new CardDetailsForm(item.CharaData));
+            if (charaListComboBox.SelectedItem is CharaComboBoxItem item) ShowFormInParentCenter(new CardDetailsForm(item.CharaData));
         }
 
-
-        private void AddItems(CharaData chara)
-        {
-            charaListComboBox.Items.Add(new CharaComboBoxItem(chara));
-            PictureBox charaIcon = new()
-            {
-                Width = 100,
-                Height = 100,
-                BackgroundImage = UnityTextureHelpers.GetCharaIcon(chara.Id),
-                BackgroundImageLayout = ImageLayout.Zoom,
-                Cursor = Cursors.Hand,
-                Tag = chara
-            };
-
-            charaIcon.Click += CharaIcon_Click;
-
-            charactersPanel.Controls.Add(charaIcon);
-        }
-
-        private void OpenFormInParentCenter(Form form)
+        private void ShowFormInParentCenter(Form form)
         {
             Form parentForm = GetParentForm(this);
 
@@ -117,6 +115,5 @@ namespace PlayerGui.Controls.CharacterInfo
             }
             else return GetParentForm(control.Parent);
         }
-
     }
 }
