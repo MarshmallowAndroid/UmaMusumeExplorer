313260e9a7d49a112381adfee6cdb6c3324041c4
Jacob Tarun <jacobtarun782@outlook.com>
2022-06-08 12:52:34 +0800

    Remove unused resources.

--- UmaMusumeExplorer/Properties/Resources.Designer.cs
+++ UmaMusumeExplorer/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace UmaMusumeExplorer.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ProgramIconImage {
-            get {
-                object obj = ResourceManager.GetObject("ProgramIconImage", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
