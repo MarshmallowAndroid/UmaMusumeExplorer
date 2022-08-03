dac323527f886aedf51775d3821875bbc6d9881f
Jacob Tarun <jacobtarun782@outlook.com>
2022-08-03 12:52:30 +0800

    My stupid code wasn't so stupid after all.

--- UmaMusumeExplorer/Controls/AssetTables.cs
+++ UmaMusumeExplorer/Controls/AssetTables.cs
@@ -10,24 +10,126 @@ namespace UmaMusumeExplorer.Controls
 {
     internal static class AssetTables
     {
-        public static IEnumerable<GameAsset> BgmGameAssets => UmaDataHelper.GetGameAssetDataRows(ga => ga.Name.StartsWith("sound/b/"));
+        private static IEnumerable<GameAsset> bgmGameFiles;
+        private static IEnumerable<RaceBgm> raceBgm;
+        private static IEnumerable<RaceBgmPattern> raceBgmPatterns;
+        private static IEnumerable<CharaData> charaDatas;
+        private static IEnumerable<CardData> cardDatas;
+        private static IEnumerable<CardRarityData> cardRarityDatas;
 
-        public static IEnumerable<RaceBgm> RaceBgm => UmaDataHelper.GetInfoDatabaseRows<RaceBgm>();
+        private static IEnumerable<TextData> charaNameTextDatas;
+        private static IEnumerable<TextData> charaNameKatakanaTextDatas;
+        private static IEnumerable<TextData> charaVoiceNameTextDatas;
+        private static IEnumerable<TextData> charaCostumeNameTextDatas;
 
-        public static IEnumerable<RaceBgmPattern> RaceBgmPatterns => UmaDataHelper.GetInfoDatabaseRows<RaceBgmPattern>();
+        public static IEnumerable<GameAsset> BgmGameAssets
+        {
+            get
+            {
+                if (bgmGameFiles == null)
+                    bgmGameFiles = UmaDataHelper.GetGameAssetDataRows(ga => ga.Name.StartsWith("sound/b/"));
 
-        public static IEnumerable<CharaData> CharaDatas => UmaDataHelper.GetInfoDatabaseRows<CharaData>();
+                return bgmGameFiles;
+            }
+        }
 
-        public static IEnumerable<CardData> CardDatas => UmaDataHelper.GetInfoDatabaseRows<CardData>();
+        public static IEnumerable<RaceBgm> RaceBgm
+        {
+            get
+            {
+                if (raceBgm == null)
+                    raceBgm = UmaDataHelper.GetInfoDatabaseRows<RaceBgm>();
 
-        public static IEnumerable<CardRarityData> CardRarityDatas => UmaDataHelper.GetInfoDatabaseRows<CardRarityData>();
+                return raceBgm;
+            }
+        }
 
-        public static IEnumerable<TextData> CharaNameTextDatas => UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 170);
+        public static IEnumerable<RaceBgmPattern> RaceBgmPatterns
+        {
+            get
+            {
+                if (raceBgmPatterns == null)
+                    raceBgmPatterns = UmaDataHelper.GetInfoDatabaseRows<RaceBgmPattern>();
 
-        public static IEnumerable<TextData> CharaNameKatakanaTextDatas => UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 182);
+                return raceBgmPatterns;
+            }
+        }
 
-        public static IEnumerable<TextData> CharaVoiceNameTextDatas => UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 7);
+        public static IEnumerable<CharaData> CharaDatas
+        {
+            get
+            {
+                if (charaDatas == null)
+                    charaDatas = UmaDataHelper.GetInfoDatabaseRows<CharaData>();
 
-        public static IEnumerable<TextData> CharaCostumeNameTextDatas => UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 5);
+                return charaDatas;
+            }
+        }
+
+        public static IEnumerable<CardData> CardDatas
+        {
+            get
+            {
+                if (cardDatas == null)
+                    cardDatas = UmaDataHelper.GetInfoDatabaseRows<CardData>();
+
+                return cardDatas;
+            }
+        }
+
+        public static IEnumerable<CardRarityData> CardRarityDatas
+        {
+            get
+            {
+                if (cardRarityDatas == null)
+                    cardRarityDatas = UmaDataHelper.GetInfoDatabaseRows<CardRarityData>();
+
+                return cardRarityDatas;
+            }
+        }
+
+        public static IEnumerable<TextData> CharaNameTextDatas
+        {
+            get
+            {
+                if (charaNameTextDatas == null)
+                    charaNameTextDatas = UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 170);
+
+                return charaNameTextDatas;
+            }
+        }
+
+        public static IEnumerable<TextData> CharaNameKatakanaTextDatas
+        {
+            get
+            {
+                if (charaNameKatakanaTextDatas == null)
+                    charaNameKatakanaTextDatas = UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 182);
+
+                return charaNameKatakanaTextDatas;
+            }
+        }
+
+        public static IEnumerable<TextData> CharaVoiceNameTextDatas
+        {
+            get
+            {
+                if (charaVoiceNameTextDatas == null)
+                    charaVoiceNameTextDatas = UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 7);
+
+                return charaVoiceNameTextDatas;
+            }
+        }
+
+        public static IEnumerable<TextData> CharaCostumeNameTextDatas
+        {
+            get
+            {
+                if (charaCostumeNameTextDatas == null)
+                    charaCostumeNameTextDatas = UmaDataHelper.GetInfoDatabaseRows<TextData>(td => td.Category == 5);
+
+                return charaCostumeNameTextDatas;
+            }
+        }
     }
 }
