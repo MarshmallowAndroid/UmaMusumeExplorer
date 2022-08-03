536d5be5774ded9a6123f855dc6eaa480cb2f765
Jacob Tarun <jacobtarun782@outlook.com>
2022-08-03 13:54:57 +0800

    Almost functional file browser.

--- UmaMusumeExplorer/Controls/FileBrowser/FileBrowserControl.Designer.cs
+++ UmaMusumeExplorer/Controls/FileBrowser/FileBrowserControl.Designer.cs
@@ -31,14 +31,24 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileBrowserControl));
             this.fileTreeView = new System.Windows.Forms.TreeView();
+            this.treeViewContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.treeViewImageList = new System.Windows.Forms.ImageList(this.components);
+            this.extractButton = new System.Windows.Forms.Button();
+            this.extractListView = new System.Windows.Forms.ListView();
+            this.nameHeader = new System.Windows.Forms.ColumnHeader();
+            this.sizeHeader = new System.Windows.Forms.ColumnHeader();
+            this.totalFileSizeLabel = new System.Windows.Forms.Label();
+            this.totalFileCountLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.treeViewContextMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // fileTreeView
             // 
             this.fileTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.fileTreeView.CheckBoxes = true;
+            this.fileTreeView.ContextMenuStrip = this.treeViewContextMenuStrip;
             this.fileTreeView.ImageIndex = 0;
             this.fileTreeView.ImageList = this.treeViewImageList;
             this.fileTreeView.Location = new System.Drawing.Point(3, 3);
@@ -47,7 +57,22 @@
             this.fileTreeView.SelectedImageIndex = 0;
             this.fileTreeView.Size = new System.Drawing.Size(434, 561);
             this.fileTreeView.TabIndex = 0;
+            this.fileTreeView.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.FileTreeView_AfterCheck);
             this.fileTreeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.FileTreeView_BeforeExpand);
+            // 
+            // treeViewContextMenuStrip
+            // 
+            this.treeViewContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem});
+            this.treeViewContextMenuStrip.Name = "treeViewContextMenuStrip";
+            this.treeViewContextMenuStrip.Size = new System.Drawing.Size(97, 26);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.AddToolStripMenuItem_Click);
             // 
             // treeViewImageList
             // 
@@ -57,14 +82,85 @@
             this.treeViewImageList.Images.SetKeyName(0, "FolderIcon.ico");
             this.treeViewImageList.Images.SetKeyName(1, "FileIcon.ico");
             // 
+            // extractButton
+            // 
+            this.extractButton.Location = new System.Drawing.Point(524, 388);
+            this.extractButton.Name = "extractButton";
+            this.extractButton.Size = new System.Drawing.Size(75, 23);
+            this.extractButton.TabIndex = 1;
+            this.extractButton.Text = "Extract";
+            this.extractButton.UseVisualStyleBackColor = true;
+            this.extractButton.Click += new System.EventHandler(this.ExtractButton_Click);
+            // 
+            // extractListView
+            // 
+            this.extractListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.extractListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameHeader,
+            this.sizeHeader});
+            this.extractListView.Location = new System.Drawing.Point(443, 3);
+            this.extractListView.Name = "extractListView";
+            this.extractListView.Size = new System.Drawing.Size(404, 341);
+            this.extractListView.TabIndex = 2;
+            this.extractListView.UseCompatibleStateImageBehavior = false;
+            this.extractListView.View = System.Windows.Forms.View.Details;
+            // 
+            // nameHeader
+            // 
+            this.nameHeader.Text = "Name";
+            this.nameHeader.Width = 325;
+            // 
+            // sizeHeader
+            // 
+            this.sizeHeader.Text = "Size";
+            this.sizeHeader.Width = 75;
+            // 
+            // totalFileSizeLabel
+            // 
+            this.totalFileSizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalFileSizeLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.totalFileSizeLabel.Location = new System.Drawing.Point(629, 349);
+            this.totalFileSizeLabel.Name = "totalFileSizeLabel";
+            this.totalFileSizeLabel.Size = new System.Drawing.Size(218, 36);
+            this.totalFileSizeLabel.TabIndex = 3;
+            this.totalFileSizeLabel.Text = "0 MB";
+            this.totalFileSizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // totalFileCountLabel
+            // 
+            this.totalFileCountLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.totalFileCountLabel.Location = new System.Drawing.Point(443, 349);
+            this.totalFileCountLabel.Name = "totalFileCountLabel";
+            this.totalFileCountLabel.Size = new System.Drawing.Size(100, 36);
+            this.totalFileCountLabel.TabIndex = 3;
+            this.totalFileCountLabel.Text = "0 files";
+            this.totalFileCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(443, 388);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
             // FileBrowserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.totalFileSizeLabel);
+            this.Controls.Add(this.totalFileCountLabel);
+            this.Controls.Add(this.extractListView);
+            this.Controls.Add(this.extractButton);
             this.Controls.Add(this.fileTreeView);
             this.Name = "FileBrowserControl";
-            this.Size = new System.Drawing.Size(705, 567);
+            this.Size = new System.Drawing.Size(850, 567);
             this.Load += new System.EventHandler(this.FileBrowserControl_Load);
+            this.treeViewContextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,5 +169,14 @@
 
         private System.Windows.Forms.TreeView fileTreeView;
         private System.Windows.Forms.ImageList treeViewImageList;
+        private System.Windows.Forms.Button extractButton;
+        private System.Windows.Forms.ListView extractListView;
+        private System.Windows.Forms.ColumnHeader nameHeader;
+        private System.Windows.Forms.ColumnHeader sizeHeader;
+        private System.Windows.Forms.Label totalFileSizeLabel;
+        private System.Windows.Forms.Label totalFileCountLabel;
+        private System.Windows.Forms.ContextMenuStrip treeViewContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.Button clearButton;
     }
 }
--- UmaMusumeExplorer/Controls/FileBrowser/FileBrowserControl.cs
+++ UmaMusumeExplorer/Controls/FileBrowser/FileBrowserControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,8 @@ namespace UmaMusumeExplorer.Controls.FileBrowser
 {
     public partial class FileBrowserControl : UserControl
     {
-        List<GameAsset> gameAssets;
+        private List<GameAsset> gameAssets;
+        private readonly List<GameAsset> selectedAssets = new();
 
         public FileBrowserControl()
         {
@@ -26,9 +28,12 @@ namespace UmaMusumeExplorer.Controls.FileBrowser
             gameAssets.Sort((ga1, ga2) => string.Compare(ga1.Name, ga2.Name));
 
             TreeNode rootNode = new("Root");
-            rootNode.Nodes.Add("dummy");
+            rootNode.Nodes.Add("");
 
             fileTreeView.Nodes.Add(rootNode);
+
+            extractListView.Columns[0].Width = (int)(extractListView.Width * 0.80f);
+            extractListView.Columns[1].Width = -2;
         }
 
         private void NodesFromPath(TreeNode sourceNode, string path)
@@ -45,24 +50,23 @@ namespace UmaMusumeExplorer.Controls.FileBrowser
             if (!sourceNode.Nodes.ContainsKey(nodeName))
                 sourceNode.Nodes.Add(nodeName, nodeName);
 
-            NodesFromPath(sourceNode.Nodes[nodeName], path.Substring(lastSlashIndex + 1));
+            NodesFromPath(sourceNode.Nodes[nodeName], path[(lastSlashIndex + 1)..]);
         }
 
         private void FileTreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
         {
-            TreeNode targetNode = e.Node;
-
-            targetNode.Nodes.Clear();
+            TreeNode expandingNode = e.Node;
+            expandingNode.Nodes.Clear();
 
             IEnumerable<GameAsset> assetList;
 
             string convertedNodePath = "";
 
-            if (targetNode.Text == "Root")
+            if (expandingNode.Text == "Root")
                 assetList = gameAssets;
             else
             {
-                convertedNodePath = targetNode.FullPath["Root/".Length..];
+                convertedNodePath = expandingNode.FullPath["Root/".Length..];
                 assetList = gameAssets.Where(ga => ga.Name.StartsWith(convertedNodePath + "/"));
             }
 
@@ -70,24 +74,23 @@ namespace UmaMusumeExplorer.Controls.FileBrowser
 
             foreach (var asset in assetList)
             {
-                string assetName = targetNode.Text == "Root" ? asset.Name : asset.Name[(convertedNodePath.Length + 1)..];
+                string assetName = expandingNode.Text == "Root" ? asset.Name : asset.Name[(convertedNodePath.Length + 1)..];
 
                 int firstSlashIndex = assetName.IndexOf('/');
-
                 if (firstSlashIndex < 1)
-                {
                     files.Add(asset);
-                    continue;
-                }
-
-                string nodeName = assetName.Substring(0, firstSlashIndex);
-                string nodeKey = convertedNodePath + '/' + nodeName;
-
-                if (!targetNode.Nodes.ContainsKey(nodeKey))
+                else
                 {
-                    targetNode.Nodes.Add(nodeKey, nodeName);
-                    targetNode.Nodes[nodeKey].Nodes.Add("dummy");
+                    string nodeName = assetName[..firstSlashIndex];
+                    string nodeKey = convertedNodePath + '/' + nodeName;
+
+                    if (!expandingNode.Nodes.ContainsKey(nodeKey))
+                    {
+                        expandingNode.Nodes.Add(nodeKey, nodeName);
+                        expandingNode.Nodes[nodeKey].Nodes.Add("");
+                    }
                 }
+
             }
 
             foreach (var file in files)
@@ -97,10 +100,122 @@ namespace UmaMusumeExplorer.Controls.FileBrowser
                 if (file.Manifest.StartsWith("manifest"))
                     fileName += ".manifest";
 
-                targetNode.Nodes.Add(file.Name, fileName);
-                targetNode.Nodes[file.Name].ImageIndex = 1;
-                targetNode.Nodes[file.Name].SelectedImageIndex = 1;
+                if (!expandingNode.Nodes.ContainsKey(file.Name))
+                {
+                    expandingNode.Nodes.Add(file.Name, fileName);
+
+                    TreeNode node = expandingNode.Nodes[file.Name];
+
+                    node.ImageIndex = 1;
+                    node.SelectedImageIndex = 1;
+                    node.Tag = file;
+                }
             }
+        }
+
+        private void AddToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode node = fileTreeView.SelectedNode;
+            if (node.FullPath == "Root") return;
+
+            GameAsset asset = (GameAsset)node.Tag;
+
+            if (asset is not null)
+            {
+                UpdateSelectedAssets(asset, node.Checked);
+            }
+            else
+            {
+                string convertedNodePath = node.FullPath[("Root/".Length)..];
+
+                IEnumerable<GameAsset> assetsToAdd = gameAssets.Where(ga => ga.Name.StartsWith(convertedNodePath));
+                int itemCount = assetsToAdd.Count();
+                if (itemCount > 1000)
+                {
+                    DialogResult confirmResult =
+                        MessageBox.Show($"Adding {itemCount} items. This might take a long time.\n\nProceed?", "Warning",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (confirmResult == DialogResult.No) return;
+                }
+
+                foreach (var item in assetsToAdd)
+                {
+                    UpdateSelectedAssets(item, node.Checked);
+                }
+            }
+
+            selectedAssets.Sort((a, b) => a.BaseName.CompareTo(b.BaseName));
+
+            totalFileCountLabel.Text = $"{selectedAssets.Count} files";
+            totalFileSizeLabel.Text = $"{selectedAssets.Sum(a => a.Length) / 1000000.0f:f2} MB";
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            selectedAssets.Clear();
+            extractListView.Items.Clear();
+
+            totalFileCountLabel.Text = $"0 files";
+            totalFileSizeLabel.Text = $"0 MB";
+        }
+
+        private void ExtractButton_Click(object sender, EventArgs e)
+        {
+            string outputDirectory = Directory.CreateDirectory("Extracted").FullName;
+
+            foreach (var assets in selectedAssets)
+            {
+                string realFilePath = Path.Combine(outputDirectory, assets.Name);
+            }
+        }
+
+        private void FileTreeView_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            //if (e.Action == TreeViewAction.Unknown) return;
+            //if (e.Node.FullPath == "Root") return;
+
+            //GameAsset asset = (GameAsset)e.Node.Tag;
+
+            //if (asset != null)
+            //{
+            //    UpdateSelectedAssets(asset, e.Node.Checked);
+            //}
+            //else
+            //{
+            //    string convertedNodePath = e.Node.FullPath[("Root/".Length)..];
+            //    foreach (var item in gameAssets.Where(ga => ga.Name.StartsWith(convertedNodePath)))
+            //    {
+            //        UpdateSelectedAssets(item, e.Node.Checked);
+            //    }
+            //}
+        }
+
+        void UpdateSelectedAssets(GameAsset asset, bool isChecked)
+        {
+            TreeNode[] matching = fileTreeView.Nodes.Find(asset.Name, true);
+            if (matching.Length > 0) matching[0].Checked = isChecked;
+
+            //if (isChecked)
+            //{
+            if (!selectedAssets.Contains(asset) && !extractListView.Items.ContainsKey(asset.Name))
+            {
+                selectedAssets.Add(asset);
+                ListViewItem item = extractListView.Items.Add(asset.Name, asset.BaseName, 0);
+                item.SubItems.Add($"{asset.Length / 1000.0f:f2} KB");
+            }
+            //}
+            //else
+            //{
+            //    selectedAssets.Remove(asset);
+            //    extractListView.Items.Remove(extractListView.Items[asset.Name]);
+            //}
+        }
+
+        void AddToListView(GameAsset asset)
+        {
+            ListViewItem item = extractListView.Items.Add(asset.Name, asset.BaseName, 0);
+            item.SubItems.Add($"{asset.Length / 1000.0f:f2} KB");
         }
     }
 }
