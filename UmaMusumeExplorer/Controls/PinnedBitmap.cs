26787c87e569d7e59b21c9a50068181cdc17343a
Jacob Tarun <jacobtarun782@outlook.com>
2022-08-13 18:55:58 +0800

    Move PinnedBitmap to common folder.

--- UmaMusumeExplorer/Controls/PinnedBitmap.cs
+++ UmaMusumeExplorer/Controls/PinnedBitmap.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Runtime.InteropServices;
 
-namespace UmaMusumeExplorer.Controls.CharacterInfo.Classes
+namespace UmaMusumeExplorer.Controls
 {
     class PinnedBitmap : IDisposable
     {
