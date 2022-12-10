d8ddf1e10985bfb20cfe0d829ca3b676d923a2e5
Jacob Tarun <jacobtarun782@outlook.com>
2022-12-10 14:03:08 +0800

    Add small skill display control.

---
+++ UmaMusumeExplorer/Controls/CharacterInfo/SkillSmall.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace UmaMusumeExplorer.Controls.CharacterInfo
+{
+    partial class SkillSmall
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.skillNameLabel = new System.Windows.Forms.Label();
+            this.iconPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // levelLabel
+            // 
+            this.levelLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.levelLabel.BackColor = System.Drawing.Color.Transparent;
+            this.levelLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.levelLabel.Location = new System.Drawing.Point(231, 3);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(42, 48);
+            this.levelLabel.TabIndex = 4;
+            this.levelLabel.Text = "Lv0";
+            this.levelLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // skillNameLabel
+            // 
+            this.skillNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.skillNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.skillNameLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.skillNameLabel.Location = new System.Drawing.Point(57, 3);
+            this.skillNameLabel.Name = "skillNameLabel";
+            this.skillNameLabel.Size = new System.Drawing.Size(168, 48);
+            this.skillNameLabel.TabIndex = 3;
+            this.skillNameLabel.Text = "Skill name";
+            this.skillNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // iconPictureBox
+            // 
+            this.iconPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.iconPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.iconPictureBox.Name = "iconPictureBox";
+            this.iconPictureBox.Size = new System.Drawing.Size(48, 48);
+            this.iconPictureBox.TabIndex = 2;
+            this.iconPictureBox.TabStop = false;
+            // 
+            // SkillSmall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.levelLabel);
+            this.Controls.Add(this.skillNameLabel);
+            this.Controls.Add(this.iconPictureBox);
+            this.Name = "SkillSmall";
+            this.Size = new System.Drawing.Size(276, 54);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label levelLabel;
+        private System.Windows.Forms.Label skillNameLabel;
+        private System.Windows.Forms.PictureBox iconPictureBox;
+    }
+}
---
+++ UmaMusumeExplorer/Controls/CharacterInfo/SkillSmall.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UmaMusumeExplorer.Game;
+
+namespace UmaMusumeExplorer.Controls.CharacterInfo
+{
+    public partial class SkillSmall : UserControl
+    {
+        private string skillName;
+        private int level;
+        private SkillRarity rarity;
+
+        public SkillSmall()
+        {
+            InitializeComponent();
+
+            skillName = Name;
+            level = 1;
+            rarity = SkillRarity.Rarity1;
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [RefreshProperties(RefreshProperties.All)]
+        public string SkillName
+        {
+            get => skillName;
+            set
+            {
+                skillNameLabel.Text = value;
+                Invalidate();
+            }
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [RefreshProperties(RefreshProperties.All)]
+        public int SkillLevel
+        {
+            get => level;
+            set
+            {
+                levelLabel.Text = $"Lv{value}";
+                Invalidate();
+            }
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [RefreshProperties(RefreshProperties.All)]
+        public SkillRarity Rarity
+        {
+            get => rarity;
+            set
+            {
+                rarity = value;
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Color[] colors = { Color.White, Color.LightSteelBlue };
+            float[] positions = { 0F, 1F };
+
+            switch (Rarity)
+            {
+                case SkillRarity.Rarity4:
+                    colors = new[]
+                    {
+                        Color.White,
+                        Color.Gold
+                    };
+                    break;
+                case SkillRarity.Rarity5:
+                    colors = new[] {
+                        Color.SpringGreen,
+                        Color.DeepSkyBlue,
+                        Color.BlueViolet,
+                        Color.DeepPink
+                    };
+                    positions = new[] { 0F, 1 / 3F, 2 / 3F, 1F };
+                    break;
+                default:
+                    break;
+            }
+
+            LinearGradientBrush linearGradientBrush = new(ClientRectangle, Color.White, Color.White, LinearGradientMode.Horizontal);
+
+            ColorBlend colorBlend = new();
+            colorBlend.Positions = positions;
+            colorBlend.Colors = colors;
+            linearGradientBrush.InterpolationColors = colorBlend;
+
+            Rectangle paddedRectangle = ClientRectangle;
+            paddedRectangle.X += 2;
+            paddedRectangle.Y += 2;
+            paddedRectangle.Width -= 4;
+            paddedRectangle.Height -= 4;
+
+            e.Graphics.FillRectangle(linearGradientBrush, ClientRectangle);
+            e.Graphics.FillRectangle(
+                new SolidBrush(Color.FromArgb(127, 255, 255, 255)),
+                paddedRectangle);
+
+            base.OnPaint(e);
+        }
+
+        public enum SkillRarity
+        {
+            Rarity1 = 1,
+            Rarity2 = 2,
+            Rarity3 = 3,
+            Rarity4 = 4,
+            Rarity5 = 5
+        }
+    }
+}
