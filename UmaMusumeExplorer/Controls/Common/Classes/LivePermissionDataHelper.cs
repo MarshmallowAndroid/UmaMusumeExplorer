c0a3156cf448a9be5e64aca11885e1847533904c
Jacob Tarun <jacobtarun782@outlook.com>
2024-03-01 11:35:44 +0800

    Don't rely on live_permission_data table anymore.

--- UmaMusumeExplorer/Controls/Common/Classes/LivePermissionDataHelper.cs
+++ UmaMusumeExplorer/Controls/Common/Classes/LivePermissionDataHelper.cs
@@ -8,20 +8,15 @@ namespace UmaMusumeExplorer.Controls.Common.Classes
     {
         public static IEnumerable<LivePermissionData> GetLivePermissionData(int musicId)
         {
-            IEnumerable<LivePermissionData> livePermissionData = AssetTables.LivePermissionDatas.Where(lpd => lpd.MusicId == musicId);
-            if (!livePermissionData.Any())
+            List<LivePermissionData> livePermissionData = AssetTables.LivePermissionDatas.Where(lpd => lpd.MusicId == musicId).ToList();
+
+            var matches = UmaDataHelper.GetManifestEntryDataRows(ga => ga.BaseName.StartsWith($"snd_bgm_live_{musicId}_chara_") && ga.BaseName.EndsWith(".awb"));
+            foreach (var audioAsset in matches)
             {
-                livePermissionData = new List<LivePermissionData>();
+                int charaId = int.Parse(audioAsset.BaseName.Remove(0, $"snd_bgm_live_{musicId}_chara_".Length)[..4]);
 
-                var matches = UmaDataHelper.GetManifestEntryDataRows(ga => ga.BaseName.StartsWith($"snd_bgm_live_{musicId}_chara_") && ga.BaseName.EndsWith(".awb"));
-
-                foreach (var audioAsset in matches)
-                {
-                    int charaId = int.Parse(audioAsset.BaseName.Remove(0, $"snd_bgm_live_{musicId}_chara_".Length)[..4]);
-
-                    if (livePermissionData is not List<LivePermissionData> list) continue;
-                    list.Add(new LivePermissionData() { MusicId = musicId, CharaId = charaId });
-                }
+                if (livePermissionData is not List<LivePermissionData> list) continue;
+                list.Add(new LivePermissionData() { MusicId = musicId, CharaId = charaId });
             }
 
             return livePermissionData;
