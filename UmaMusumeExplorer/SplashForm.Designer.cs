a5947db79a58123dd7e879901c9240c9b0c959b8
Jacob Tarun <jacobtarun782@outlook.com>
2024-04-12 16:01:08 +0800

    Disable splash window icon from appearing on the taskbar.

--- UmaMusumeExplorer/SplashForm.Designer.cs
+++ UmaMusumeExplorer/SplashForm.Designer.cs
@@ -28,49 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            titleLabel = new System.Windows.Forms.Label();
-            loadingLabel = new System.Windows.Forms.Label();
-            loadingProgressBar = new System.Windows.Forms.ProgressBar();
+            titleLabel = new Label();
+            loadingLabel = new Label();
+            loadingProgressBar = new ProgressBar();
             SuspendLayout();
             // 
             // titleLabel
             // 
             titleLabel.AutoSize = true;
-            titleLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            titleLabel.Location = new System.Drawing.Point(111, 34);
+            titleLabel.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            titleLabel.Location = new Point(111, 34);
             titleLabel.Name = "titleLabel";
-            titleLabel.Size = new System.Drawing.Size(363, 45);
+            titleLabel.Size = new Size(363, 45);
             titleLabel.TabIndex = 0;
             titleLabel.Text = "Uma Musume Explorer";
             // 
             // loadingLabel
             // 
-            loadingLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            loadingLabel.Location = new System.Drawing.Point(12, 79);
+            loadingLabel.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            loadingLabel.Location = new Point(12, 79);
             loadingLabel.Name = "loadingLabel";
-            loadingLabel.Size = new System.Drawing.Size(561, 30);
+            loadingLabel.Size = new Size(561, 30);
             loadingLabel.TabIndex = 1;
             loadingLabel.Text = "Reading tables...";
-            loadingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            loadingLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // loadingProgressBar
             // 
-            loadingProgressBar.Location = new System.Drawing.Point(111, 112);
+            loadingProgressBar.Location = new Point(111, 112);
             loadingProgressBar.Name = "loadingProgressBar";
-            loadingProgressBar.Size = new System.Drawing.Size(363, 23);
+            loadingProgressBar.Size = new Size(363, 23);
             loadingProgressBar.TabIndex = 2;
             // 
             // SplashForm
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(585, 169);
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(585, 169);
             Controls.Add(loadingProgressBar);
             Controls.Add(loadingLabel);
             Controls.Add(titleLabel);
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            FormBorderStyle = FormBorderStyle.None;
             Name = "SplashForm";
-            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "SplashForm";
             Load += SplashForm_Load;
             ResumeLayout(false);
